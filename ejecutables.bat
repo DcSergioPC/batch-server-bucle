f2443dac55bc29dac8c56a2d8e9435d1386f804d
DcSergioPC <89735575+DcSergioPC@users.noreply.github.com>
2021-11-12 14:29:43 -0300

    Update ejecutables.bat

--- ejecutables.bat
+++ ejecutables.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 echo This script will keep your server running even after crashing!
-title Minecraft 1.17.1
+title Spigot 1.17.1
 :StartServer
 start /b /wait /high java -Xmx2096M -jar server.jar --nogui --port 15235 -o true
 goto StartServer
